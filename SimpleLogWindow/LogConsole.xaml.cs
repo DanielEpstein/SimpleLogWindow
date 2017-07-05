137dd989377fe03bef1182ef531b142f0f67b2e3
Daniel Epstein <knoziggeh@hotmail.com>
2017-07-06 01:31:34 +1000

    170706 - First Implementation

---
+++ SimpleLogWindow/LogConsole.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleLogWindow
+{
+    public partial class LogConsole : Window
+    {
+        private MainWindow mainwindow;
+
+        public LogConsole() { }
+
+        public LogConsole(MainWindow mainwindow)
+        {
+            InitializeComponent();
+
+            this.mainwindow = mainwindow;
+        }
+
+        public void Log(string text)
+        {
+            this.OutputBox.AppendText(text + "\n");
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+            base.OnClosing(e);
+        }
+
+        private void scrollviewer_Messages_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            ScrollViewer sv = sender as ScrollViewer;
+            bool AutoScrollToEnd = true;
+            if (sv.Tag != null)
+            {
+                AutoScrollToEnd = (bool)sv.Tag;
+            }
+            if (e.ExtentHeightChange == 0)
+            {
+                AutoScrollToEnd = sv.ScrollableHeight == sv.VerticalOffset;
+            }
+            else
+            {
+                if (AutoScrollToEnd)
+                {
+                    sv.ScrollToEnd();
+                }
+            }
+            sv.Tag = AutoScrollToEnd;
+            return;
+        }
+    }
+}
---
+++ SimpleLogWindow/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleLogWindow
+{
+    public partial class MainWindow : Window
+    {
+        int count = 0;
+        LogConsole console = new LogConsole();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartLogConsole();
+        }
+
+        public void StartLogConsole()
+        {
+            MainWindow mainwindow = this;
+            console = new LogConsole(mainwindow);
+            // console.Left = this.Left + 0;
+            // console.Top = this.Top + 0;
+            console.Visibility = Visibility.Collapsed;
+        }
+
+        private void buttonLogConsole_Click(object sender, RoutedEventArgs e)
+        {
+            if (console.Visibility != Visibility.Visible)
+            {
+                console.Visibility = Visibility.Visible;
+                console.Scroller.ScrollToEnd();
+                return;
+            }
+            console.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddLogItem_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+            console.Log("Log Entry: " + count);
+            
+        }
+    }
+}
